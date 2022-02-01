b3b88421c5df0c1a0766564366b49f0691180d2e
nathanelmer <nelmer12@gmail.com>
2022-02-01 11:50:10 -0600

    added ability to save a listing

--- src/components/listings/Listings.js
+++ src/components/listings/Listings.js
@@ -4,13 +4,17 @@ import "../listings/Listing.css"
 
 export const Listings = () => {
     const [listings, setListings] = useState([])
+    const [savedListing, setSavedListing] = useState({})
+    const currentUser = localStorage.getItem("user")
 
     useEffect(() => {
         fetchIt("http://localhost:8088/listings")
             .then(listingData => setListings(listingData))
     },[])
 
-
+  const sendSavedListing = (listing) => {
+    fetchIt("http://localhost:8088/savedListings", "POST", JSON.stringify(listing))
+  }
 
     return <>
         <h2>Listings</h2>
@@ -23,6 +27,14 @@ export const Listings = () => {
                             <p>$ {list.price.toLocaleString()}</p>
                             <p>{list.address}</p>
                             <p>{list.bedrooms} bedroom(s)/ {list.bathrooms} bathroom(s)</p>
+                            <button onClick={() => {
+                                const copy = {...savedListing}
+                                copy.userId = parseInt(currentUser)
+                                copy.listingId = list.id
+                                copy.note = "You have no notes for this listing!"
+                                setSavedListing(copy)
+                                sendSavedListing(copy)
+                            }}>Save</button>
                         </div>})
             }
         </section>
